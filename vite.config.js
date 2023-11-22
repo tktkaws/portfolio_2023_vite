b67d1b3fc15745d1456f2ceb5a49d498382328e3
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-11-22 09:11:17 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+﻿import { defineConfig } from "vite";
+import { resolve } from "path";
+
+const root = "src";
+
+export default defineConfig({
+  root,
+  base: "/",
+  publicDir: "./public",
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        index: resolve(root, "index.html"),
+        about: resolve(root, "about.html"),
+        works01: resolve(root, "works01.html"),
+        works02: resolve(root, "works02.html"),
+        works03: resolve(root, "works03.html"),
+        works04: resolve(root, "works04.html"),
+      },
+    },
+  },
+  server: {
+    host: true,
+  },
+  css: {
+    devSourcemap: true,
+  },
+});
